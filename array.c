ea4ceccaa5f903f7b1d1380bc8250bb2263b2592
sayali-sawant07 <sayali.jsawant90@gmail.com>
2025-07-04 14:15:46 +0000

    4/7

---
+++ array.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int i=0;
+    int marks[5];
+    marks[0]=80;
+    marks[1]=88;
+    marks[2]=75;
+    marks[3]=85;
+    marks[4]=90;
+    
+    for(i=0;i<5;i++){
+        printf("%d\n", marks[i]);
+    }
+   
+}
