a055ecd99340ffd2769879fd4d7c58fca1b639db
sayali-sawant07 <sayali.jsawant90@gmail.com>
2025-07-18 14:19:35 +0000

    18/7

--- strcpy.c
+++ strcpy.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-    char ch[10]={'t','p','o','i','n','t','t','e','c,','h',/0'};
+    char ch[10]={'t','p','o','i','n','t','t','e','c','h','/0'};
     char ch2[10];
-    strcpy()
+    strcpy(ch2,ch);
+printf("value of second string is %s", ch2, ch);
 }
