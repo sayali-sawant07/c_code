7b5e1b5d42f5b5363cde3dd70a1bee308bbca082
sayali-sawant07 <sayali.jsawant90@gmail.com>
2025-07-18 14:16:18 +0000

    18/7

---
+++ strcpy.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char ch[10]={'t','p','o','i','n','t','t','e','c,','h',/0'};
+    char ch2[10];
+    strcpy()
+}
---
+++ strlen.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char ch[20]={'t','p','o','i','n','t','t','e','c','h','/0'};
+    printf("Lenght of string is %d", strlen(ch));
+}
